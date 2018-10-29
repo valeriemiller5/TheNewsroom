64468388f631959251302dc7b8f19d987c056f34
vflores <valerie.flores64@gmail.com>
2018-10-29 16:06:22 -0700

    added an env to store mLab user and password

--- server.js
+++ server.js
@@ -12,11 +12,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// ***********MONGOOSE SETUP FOR HEROKU DEPLOYMENT***********
 // mongoose.connect("mongodb://localhost/newsdatabase", { useNewUrlParser: true });
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsdatabase";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+// ******************************************************************
 
 // Set Handlebars
 var exphbs = require("express-handlebars");
